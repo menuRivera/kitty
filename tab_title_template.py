8e2f7ef09541d086b761d88a2d4f0f9d236f7861
Manuel Rivera <menu.rivera@gmail.com>
2025-08-10 20:27:46 -0600

    feat(tab_title): refactor on get_tab_title_template()

--- tab_title_template.py
+++ tab_title_template.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 
-def get_tab_title_template():
+def get_tab_title_template(is_active):
     bg = "{fmt.bg.default}"
-    property = "tab_title_template"
+
+    property = "active_tab_title_template" if is_active \
+        else "tab_title_template"
+
     title = "{title if title == '~' else tab.active_wd.split('/')[-1]}"
 
-    return f'{property} "{bg} {title}  "'
-
-
-def get_active_tab_title_template():
-    bg = "{fmt.bg.default}"
-    property = "active_tab_title_template"
-    title = "{title if title == '~' else tab.active_wd.split('/')[-1]}"
-
-    return f'{property} "{bg}<{title}> "'
+    return f'{property} "{bg}<{title}> "' if is_active \
+        else f'{property} "{bg} {title}  "'
 
 
 if __name__ == "__main__":
-    print(get_tab_title_template())
-    print(get_active_tab_title_template())
+    print(get_tab_title_template(is_active=True))
+    print(get_tab_title_template(is_active=False))
